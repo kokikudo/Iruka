ecb16b4e0527f426e7f80cfaf6dfb947ae2f90af
kokikudo <massanmesu@icloud.com>
2021-04-18 21:52:09 +0900

    商品ページを8割作成 残り作業 保存ボタンをタップすると未入力の必須項目がないか確認する処理 なければデータを保存する処理 新規登録か再評価かの判断するBool値の設定

--- Iruka/ItemEditPageViewController.swift
+++ Iruka/ItemEditPageViewController.swift
@@ -7,23 +7,222 @@
 
 import UIKit
 
-class ItemEditPageViewController: UIViewController {
-
+class ItemEditPageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //Propaties
+    @IBOutlet weak var registrationTimeLabel: UILabel!
+    @IBOutlet weak var registrationTimeText: UILabel!
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var wordCountLabel: UILabel!
+    private let itemNameTextMaxCount = 40
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var priceText: UITextField!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var impressionLabel: UILabel!
+    @IBOutlet weak var changeButton: UISwitch!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var impressionText: UITextView!
+    @IBOutlet weak var impressionWordCountLabel: UILabel!
+    private let impressionMaxCount = 150
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        nameText.delegate = self
+        priceText.delegate = self
+        impressionText.delegate = self
+        
+        // タップでキーボードを下げる
+        downKeyboardInTap()
+        // 金額入力のキーボードにリターンキーを追加
+        addReturnBotton()
+        // 感想文のプレースホルダーの状態
+        impressionText.text = "感想を入力..."
+        impressionText.textColor = UIColor.lightGray
+        // 現在日時を取得
+        let datefomatter = DateFormatter()
+        datefomatter.dateStyle = .long
+        datefomatter.timeStyle = .none
+        datefomatter.locale = Locale(identifier: "ja_JP")
+        registrationTimeText.text = datefomatter.string(from: Date())
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func downKeyboardInTap() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
     }
-    */
-
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    // 金額と感想のキーボードにリターンボタンを追加
+    func addReturnBotton() {
+        let priceTextToolber: UIToolbar = UIToolbar()
+        let impressionTextToolber: UIToolbar = UIToolbar()
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let priceTextDone = UIBarButtonItem(title: "完了",
+                                            style: .done,
+                                            target: self,
+                                            action: #selector(priceTextShouldReturn))
+        priceTextToolber.items = [space, priceTextDone]
+        priceTextToolber.sizeToFit()
+        priceText.inputAccessoryView = priceTextToolber
+        
+        let impressionTextDone = UIBarButtonItem(title: "完了",
+                                                 style: .done,
+                                                 target: self,
+                                                 action: #selector(impressionTextShouldReturn))
+        impressionTextToolber.items = [space, impressionTextDone]
+        impressionTextToolber.sizeToFit()
+        impressionText.inputAccessoryView = impressionTextToolber
+    }
+    
+    // 画像がタップされてたらアラート起動。カメラ撮影かライブラリ選択かを選び、それぞれの処理を実行。
+    @IBAction func selectImage(_ sender: UITapGestureRecognizer) {
+        
+        // アラートインスタンス
+        let alertController: UIAlertController = UIAlertController(title: "写真を選択", message: "選択してください", preferredStyle: .alert)
+        
+        // カメラ撮影
+        let cameraAction: UIAlertAction = UIAlertAction(title: "カメラで撮影", style: .default) { (action) in
+            
+            let cameraPickerController = UIImagePickerController()
+            cameraPickerController.sourceType = .camera
+            cameraPickerController.delegate = self
+            self.present(cameraPickerController, animated: true, completion: nil)
+        }
+        
+        // ライブラリ
+        let libralyAction: UIAlertAction = UIAlertAction(title: "ライブラリから選択", style: .default) { (action) in
+            
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.sourceType = .photoLibrary
+            imagePickerController.delegate = self
+            self.present(imagePickerController, animated: true, completion: nil)
+        }
+        
+        // キャンセル
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: {(action: UIAlertAction!) -> Void in
+            self.dismiss(animated: true, completion: nil)
+        })
+        
+        // 各アラートアクションを追加
+        alertController.addAction(cameraAction)
+        alertController.addAction(libralyAction)
+        alertController.addAction(cancelAction)
+        
+        // アラートコントローラーへ移動
+        present(alertController, animated: true, completion: nil)
+    }
+    // デリゲートメソッド。キャンセルしたら何もせず画面を閉じる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    // デリゲートメソッド。画像を取得できたらプロパティを更新する。
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        else {
+            fatalError("画像の取得に失敗しました")
+        }
+        photoImage.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
+// TextFieldのデリゲートとその他のメソッド
+extension ItemEditPageViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.tag == 2 {
+            textField.text = ""
+        }
+    }
+    
+    // リターンキーでキーボードを下げる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    // 金額入力後、完了ボタンを押すと通貨記号が付いたフォーマットに変わりキーボードを閉じる
+    @objc func priceTextShouldReturn() -> Bool {
+        // 金額に単位を追加
+        guard let price = Float(priceText.text!) else {
+            fatalError("数字以外の文字が入力されました")
+        }
+        // 単位を追加するフォーマットを作成
+        let priceFormatter = NumberFormatter()
+        priceFormatter.numberStyle = .currency  // 通貨記号をつける
+        // セット
+        priceText.text = priceFormatter.string(from: NSNumber(value: price))
+        
+        priceText.resignFirstResponder()
+        return true
+    }
+    
+    // 文字数制限
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // 入力を反映したテキストを取得
+        let resultText: String = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        return resultText.count <= itemNameTextMaxCount
+    }
+    
+    // 現在の文字数表示
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if textField.tag == 1 {
+            wordCountLabel.text = "( \(textField.text!.count) / \(itemNameTextMaxCount) )"
+        }
+    }
+    
+}
+
+// textView(感想入力欄)のデリゲートとその他のメソッド
+extension ItemEditPageViewController: UITextViewDelegate {
+    
+    // 追加した完了ボタンを押すとキーボードが下がる
+    @objc func impressionTextShouldReturn() -> Bool {
+        impressionText.resignFirstResponder()
+        return true
+    }
+    
+    // 文字数制限
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let resultText: String = (textView.text! as NSString).replacingCharacters(in: range, with: text)
+        return resultText.count <= impressionMaxCount
+    }
+    
+    // 現在の文字数表示
+    func textViewDidChange(_ textView: UITextView) {
+        impressionWordCountLabel.text = "( \(textView.text!.count) / \(impressionMaxCount) )"
+    }
+    
+    // プレースホルダー
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "(必須。150文字まで)"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+}
+
+
---
+++ Iruka/RatingControl.swift
@@ -0,0 +1,135 @@
+//
+//  RatingControl.swift
+//  Iruka
+//
+//  Created by kudo koki on 2021/04/15.
+//
+
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Propaties
+    private var ratingButtons = [UIButton]() // 評価の星。5つあるためリストで格納
+    var rating = 0 { // 星の数。更新時に設定値より下の配列の星をセレクト状態にする関数を実行
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
+    // 星のサイズ。storyboardのinspecter上でいじれるようになる。
+    // 値が更新された場合はまたボタンを作り直す必要がある
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    // 星の数
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    //MARK: Initalization
+    override init(frame: CGRect) { // CGRect: 描画するオブジェクトの位置とサイズを表現する型
+        super.init(frame: frame)
+        setupButtons() // ボタン生成
+    }
+     // required initが定義されてるNSCodingプロトコルがUIViewで定義されており、UIViewを継承しているUIStackViewでも定義する
+     // 通常、初期化しない場合は不要だが、指定イニシャライザを定義する場合はこちらも定義するのがるーるらしい
+    required init(coder: NSCoder) { // NSCoder: クラスをアーカイブに保存するための型
+        super.init(coder: coder)
+        setupButtons() // こっちにも同じ処理
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        // 配列内にあるボタンの要素数から評価点を算出
+        // 押されたボタンの要素数を取得
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        // 要素数に1を加算
+        let selectedRating = index + 1
+        // ratingと同じ数の場合（同じ星をタップした場合）、評価をクリアする
+        if selectedRating == rating {
+            rating = 0
+        } else { // そうでない場合はratingを更新
+            rating = selectedRating
+        }
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        
+        // ボタンを新規作成するたびに、古いボタンを削除する必要がある。
+        for button in ratingButtons {
+            // ようわからんが、削除する時はこのコードを書く必要がある
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll() // プロパティの星を全削除
+        
+        // ボタンのイメージ画像をアセットから呼び出す
+        let bundle = Bundle(for: type(of: self)) // バンドルをメインバンドルに設定
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        // 評価の星を作成
+        for index in 0..<starCount {
+            // ボタンを作成
+            let button = UIButton()
+            button.setImage(emptyStar, for: .normal) // 通常の星のイメージ画像
+            button.setImage(filledStar, for: .selected) // 選択された星のイメージ
+            button.setImage(highlightStar, for: .highlighted) // 洗濯中（タップ中）のイメージ
+            button.setImage(highlightStar, for: [.highlighted, .selected]) // 選択中、選択後のイメージ
+            
+            // 制約を追加
+            //button.translatesAutoresizingMaskIntoConstraints = false // AutoLayoutを無効
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            // アクセシビリティ。現在設定している評価点を出力
+            button.accessibilityLabel = "Set \(index + 1) star rating"
+            
+            // ボタンを押された時にアクションメソッドratingButtonTappedが実行されるように設定
+            // .addTarget()は対象のクラスインスタンスに何かしらのアクションを起こしたときに関数を実行するようにする
+            // actionには実行したい関数をセレクタという形式で渡す。関数の先頭に@objcを入れる。
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            addArrangedSubview(button) //　設定したボタンを追加
+            ratingButtons.append(button) // プロパティにも追加
+        }
+        //updateButtonSelectionStates() // タップした星より左にある星をセレクト状態
+    }
+    
+    // タップされたボタンより左のボタンを全てセレクト状態にする
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+            
+            // アクセシビリティの文章。評価点と星が一緒場合のヒント文
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            } else {
+                hintString = nil
+            }
+            
+            // アクセシビリティの文章。評価点によって変わる。
+            let valueString: String
+            switch rating {
+            case 0:
+                valueString = "No rating set."
+            default:
+                valueString = "\(rating) stars set."
+            }
+            
+            // アクセシビリティの文章をセット
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
+        }
+    }
+    
+}
