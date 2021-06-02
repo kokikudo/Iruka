b0283fe1a028cce64ba1becf5972b8d25482d8a3
kokikudo <massanmesu@icloud.com>
2021-06-02 13:19:12 +0900

    金額の完了ボタンを削除。感想文の完了ボタンを削除する前に保存ボタンをバーの右上に配置できないか試す

--- Iruka/MyUINavigationController.swift
+++ Iruka/MyUINavigationController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MyUIViewController: UINavigationController {
+class MyUINavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
